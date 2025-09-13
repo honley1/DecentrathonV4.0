e3da3f29f396d4bc7f039ab6fa325437c361f5e3
Honley <mfhonley@gmail.com>
2025-09-13 18:54:14 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+import router as api
+
+app = FastAPI()
+
+app.include_router(api)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
